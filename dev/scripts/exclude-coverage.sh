ebe7f45b7e0ecb8a589b02eddb431bf3afd811be
Darien Hernandez <dohernandez@gmail.com>
2024-12-10 14:59:46 +0100

    patch: Patch exclude coverage script
    The issue here is related to the way sed is interpreting the paths you're passing, particularly with the -i option (which performs in-place editing) on macOS.

--- dev/scripts/exclude-coverage.sh
+++ dev/scripts/exclude-coverage.sh
@@ -5,8 +5,20 @@ if [ ! -f ./integration-exclude.coverpkg ]; then
   exit 0
 fi
 
+# Check if the system is macOS or Linux
+OS_TYPE=$(uname -s)
+
 # Process each non-empty, non-comment line in exclude-coverage.txt
 grep -v -E '^\s*#|^\s*$' ./integration-exclude.coverpkg | while read -r p || [ -n "$p" ]; do
-  # Use sed to remove matching lines from the coverage file
-  sed -i '' "/$(echo "$p" | sed 's/[&/\]/\\&/g')/d" ./integration.coverprofile
+  # Escape special characters for sed
+  ESCAPED_PATH=$(echo "$p" | sed 's/[&/\]/\\&/g')
+
+  # On macOS, use -i '' for in-place editing; on Linux, use -i without any argument
+  if [[ "$OS_TYPE" == "Darwin" ]]; then
+    # macOS: use -i '' for in-place editing
+    sed -i '' "/$ESCAPED_PATH/d" ./integration.coverprofile
+  else
+    # Linux: use -i without any argument for in-place editing
+    sed -i "/$ESCAPED_PATH/d" ./integration.coverprofile
+  fi
 done
