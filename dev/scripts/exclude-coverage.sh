e865326be2856d54e9f13cbb0d3c9389c9888beb
Darien Hernandez <dohernandez@gmail.com>
2024-12-10 11:23:58 +0100

    patch: exclude-coverage.sh

--- dev/scripts/exclude-coverage.sh
+++ dev/scripts/exclude-coverage.sh
@@ -2,8 +2,7 @@
 
 # Check if exclude-coverage.txt exists
 if [ ! -f ./exclude-coverage.txt ]; then
-  echo "exclude-coverage.txt not found. Exiting."
-  exit 1
+  exit 0
 fi
 
 # Process each line in exclude-coverage.txt
