7ceba5760b47de1778f55b9c5cc66ace6193f4f6
Darien Hernandez <dohernandez@gmail.com>
2024-12-15 00:41:22 +0100

    feat: Add health feature test

--- test/feature/health.go
+++ test/feature/health.go
@@ -22,20 +22,35 @@ func NewHealth(baseURL string) *Health {
 func (h *Health) RegisterSteps(s *godog.ScenarioContext) {
 	s.Step(`^I check server is up and running$`, h.iCheckServerIsUpAndRunning)
 	s.Step(`^It should be up and running$`, h.itShouldBeUpAndRunning)
+
+	s.Step(`^I check server is healthy$`, h.iCheckServerIsHealthy)
+	s.Step(`^It should be healthy$`, h.itShouldBeHealthy)
 }
 
 func (h *Health) iCheckServerIsUpAndRunning() error {
+	return h.iCheckSetup("/")
+}
+
+func (h *Health) iCheckSetup(method string) error {
 	if err := h.CheckUnexpectedOtherResponses(); err != nil {
 		return fmt.Errorf("unexpected other responses for previous request: %w", err)
 	}
 
 	h.Reset()
 	h.WithMethod(http.MethodGet)
-	h.WithURI("/")
+	h.WithURI(method)
 
 	return nil
 }
 
 func (h *Health) itShouldBeUpAndRunning() error {
+	return h.ExpectResponseStatus(200)
+}
+
+func (h *Health) iCheckServerIsHealthy() error {
+	return h.iCheckSetup("/health")
+}
+
+func (h *Health) itShouldBeHealthy() error {
 	return h.ExpectResponseStatus(200)
 }
